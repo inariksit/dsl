3e7bdc8183dbecb363d570f264e609e64be8ba81
Wong Meng Weng <mwwong@smu.edu.sg>
2020-09-12 17:08:13 +0800

    add utility to show syntax error lines; improve langstrings

---
+++ bnfc/showbug/Main.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE QuasiQuotes                      #-}
+
+module Main where
+
+import System.Environment ( getArgs, getProgName )
+import System.Exit        ( exitFailure, exitSuccess )
+import System.IO
+import Control.Monad      ( when )
+import Data.Maybe         ( fromMaybe )
+import Text.RE.PCRE
+import Data.Function
+
+-- when you run l4 test1.l4 > stdoutfile 2> stderrfile
+-- you might get "syntax error at line M, column N before ...
+-- so, if you call showbug origfile stdoutfile stderrfile
+-- you will get a view of the origfile where the error message complains.
+-- note: what ought to be stderr is in stdout
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["--help"] -> usage
+    [origfile, stdoutfile, stderrfile] -> do
+      stderr <- return.lines =<< readFile stderrfile
+      stdout <- return.lines =<< readFile stdoutfile
+      orig   <- return.lines =<< readFile origfile
+      let coords = getErrorCoordinates $ stdout ++ stderr
+      case coords of
+        Nothing -> mapM putStrLn stdout                 >> exitSuccess
+        Just xy -> mapM putStrLn (showAtCoords orig xy) >> exitFailure
+
+usage = do
+  putStrLn "showbug test1.l4 out/test1.out out/test1.err"
+      
+getErrorCoordinates inlines = do
+  mt <- matchedText (flip (?=~) syntaxError (unlines inlines))
+  let ms = matches $ mt *=~ [re|\d+|]
+  case ms of
+    [x,y] -> return (read x, read y, mt)
+    otherwise -> Nothing
+  where
+    syntaxError = [re|syntax error at line \d+, column \d+.*|]
+
+showAtCoords origlines (line,col,errormessage) =
+  [ "* " ++ errormessage
+  , "| " ++ origlines !! (line-2)
+  , "| " ++ origlines !! (line-1)
+  , "  " ++ (nspaces $ col - 1) ++ "^"
+  , "| " ++ origlines !! (line-0)
+  ]
+  where
+    nspaces n = take n $ repeat ' '
+
